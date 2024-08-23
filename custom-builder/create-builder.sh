fb92bc4a151b523158a838baf9a68d4c42b6a1c5
Aaron Fleckenstein <aaron.fleckenstein@daveramsey.com>
2024-08-23 09:12:16 -0500

    Script works, figure out why the yarn one does not

---
+++ custom-builder/create-builder.sh
@@ -0,0 +1,4 @@
+docker buildx build --platform linux/amd64,linux/arm64 -f build.Dockerfile -t a1flecke/my-builder-builder --push  .
+docker buildx build --platform linux/amd64,linux/arm64 -f run.Dockerfile -t a1flecke/my-builder-run --push  .
+# create builder
+pack builder create a1flecke/my-builder:jammy --config ./builder.toml
